b186b92bebfa5af76e08fbe323301219293d164f
Halil <dospatliyski@gmail.com>
2025-09-26 11:16:08 +0300

    etert

---
+++ 01. Tipove danni/01. AreaSquare/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01._AreaSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Лице на квадрат
+
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+
+            double S = a * a;
+            Console.WriteLine($"S={S:f2} kv.sm");
+
+
+        }
+    }
+}
---
+++ 01. Tipove danni/02. AreaRectangle/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._AreaRectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+
+            double s = a * b;
+            Console.WriteLine($"S={s}");
+
+        }
+    }
+}
