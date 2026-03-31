17444ce80b26e0a0d434695b3cbade420408b1b2
Halil <dospatliyski@gmail.com>
2026-03-31 14:01:28 +0300

    ewew

---
+++ 03. ForLoops/07.3 - z6 - MagicNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _07._3___z6___MagicNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int magicN = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    for (int k = 1; k < 9; k++)
+                    {
+                        for (int l = 1; l < 9; l++)
+                        {
+                            for (int m = 1; m < 9; m++)
+                            {
+                                for (int n = 1; n < 9; n++)
+                                {
+                                    if (i*j*k*l*m*n==magicN)
+                                    {
+                                        Console.WriteLine($"{i}{j}{k}{l}{m}{n}");
+                                        counter++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Count: {counter}");
+        }
+    }
+}
---
+++ 03. ForLoops/4. VlojenCikal/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _4._VlojenCikal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"i={i}");
+                for (char j = 'a'; j < 'f'; j++)
+                {                   
+                    Console.WriteLine($"j={j}");                   
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
